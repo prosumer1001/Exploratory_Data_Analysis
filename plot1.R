9cd65c5c16a69290183c9efda55be78651efae22
prosumer1001 <prosumer1001@gmail.com>
2020-07-25 15:12:45 +0900

    Completed code for plot1.R and completed barplot in Base R to answer Question 1

--- plot1.R
+++ plot1.R
@@ -6,7 +6,33 @@
 ## Using the base plotting system, make a plot showing the total PM2.5 
 ## emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
+## Answer to the question:  Yes, the total emissions from PM2.5 has decreased in the U.S. from 1999 to 2008.
 ## Import Data
+## PM2.5
 install.packages("rio")
 library(rio)
-data <- import("summarySCC_PM25.rds")
+data01 <- import("summarySCC_PM25.rds")
+
+## Emissions source classification
+data02 <- import("Source_Classification_Code.rds")
+
+## Aggregate emissions by year
+emissionsAggregate <- aggregate(Emissions ~ year, data = data01, FUN = sum)
+
+## Open png machine to create plot1.png
+png(filename = "plot1.png", width = 540, height = 540, units = "px")
+
+## Print numeric values in fixed notation
+#options(scipen = 10)
+
+barplot(
+        (emissionsAggregate$Emissions)/10^6,
+        names.arg=emissionsAggregate$year,
+        xlab="Year",
+        ylab="PM2.5 Emissions (Millions of Tons)",
+        main="Total PM2.5 Emissions From All US Sources"
+)
+
+## Close png machine
+dev.off()
+
