e909b68439b884ae4cd2cc6bf422f2a6ba4cc9be
prosumer1001 <62540454+prosumer1001@users.noreply.github.com>
2020-07-25 14:43:27 +0900

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,12 @@
-# Question 1, plot1
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, 
-     # make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+## Coursera, Course.  Exploratory Data Analysis
+## Coursera (https://www.coursera.org/learn/exploratory-data-analysis/peer/b5Ecl/course-project-2)
+
+## Question 1, plot1
+## Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+## Using the base plotting system, make a plot showing the total PM2.5 
+## emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+
+## Import Data
+install.packages("rio")
+library(rio)
+data <- import("summarySCC_PM25.rds")
